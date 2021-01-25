4aaf4bd936c355cdf15c1b3cd24cdfc02b0dc714
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2021-01-26 00:58:38 +0900

    プロフィール更新

--- portfolio/data/siteConfig.js
+++ portfolio/data/siteConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-  siteTitle: 'Hi! I\'m Francisco!',
-  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  siteTitle: 'ONODA',
+  siteDescription: `My online curriculum`,
   keyWords: ['gatsbyjs', 'react', 'curriculum'],
-  authorName: 'Francisco Santos',
-  twitterUsername: '_franciscodf',
-  githubUsername: 'santosfrancisco',
-  authorAvatar: '/images/avatar.jpeg',
+  authorName: 'Onoda Yoshiaki',
+  twitterUsername: '',
+  githubUsername: 'GangDuo',
+  authorAvatar: '/images/avatar.png',
   authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
     In 2012 I started working as a support technician and I approached the developers.
     In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
@@ -24,11 +24,23 @@ module.exports = {
       level: 50
     },
     {
+      name: 'C#',
+      level: 50
+    },
+    {
+      name: 'C',
+      level: 50
+    },
+    {
       name: 'NodeJs',
       level: 40
     },
     {
       name: 'React',
+      level: 60
+    },
+    {
+      name: 'MySQL',
       level: 60
     },
     {
@@ -40,69 +52,94 @@ module.exports = {
   jobs: [
     /* more jobs here */
     {
-      company: "Gympass",
+      company: "HUMPTYDUMPTY",
       begin: {
-        month: 'sep',
-        year: '2019'
-      },
-      duration: null,
-      occupation: "Frontend developer",
-      description: "I am part of the Corporate team, responsible for the development and maintenance of the employee management platform, giving more and more autonomy to partner companies."
-  
-    },  {
-      company: "Lendico",
-      begin: {
-        month: 'apr',
-        year: '2018'
-      },
-      duration: null,
-      occupation: "Frontend developer",
-      description: "I integrate the Frontend team responsible for developing and maintaining the online lending platform."
-  
-    }, {
-      company: "Anapro",
-      begin: {
-        month: 'dec',
-        year: '2016'
-      },
-      duration: '1 yr e 5 mos',
-      occupation: "Fullstack developer",
-      description: "Development and maintenance, corrective and preventive, of web applications for the real estate market."
-    }, {
-      company: "Anapro",
-      begin: {
-        month: 'set',
+        month: '12',
         year: '2012'
       },
-      duration: '4 yrs e 3 mos',
-      occupation: "Support Technician",
-      description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
-  
+      duration: null,
+      occupation: "Fullstack developer",
+      description: "私は小売チェーン店の本社業務効率化アプリケーションの企画・開発・保守を担当し、ソフトウェアで退屈な時間から人類を解放し、私達の生活を進歩させます。"
+
+    },  {
+      company: "Logiway",
+      begin: {
+        month: '10',
+        year: '2009'
+      },
+      duration: '7 ヶ月',
+      occupation: "Fullstack developer",
+      description: "私は物流センター管理システムの個人向け配送サービスの企画・開発・保守を担当しました。"
+
+    }, {
+      company: "Yokoyama",
+      begin: {
+        month: '8',
+        year: '2006'
+      },
+      duration: '3 年 1 ヶ月',
+      occupation: "Fullstack developer",
+      description: "複数の美容・健康ECサイトの構築。受注管理システムの企画、複数サイトの商品一元管理システムの企画・開発・保守を担当しました。"
+    }, {
+      company: "computron",
+      begin: {
+        month: '4',
+        year: '2003'
+      },
+      duration: '3 年 4 ヶ月',
+      occupation: "Embedded software developer",
+      description: "通信デバイスのテスターと開発・保守を担当しました。"
+
     },
   ],
   portifolio: [
     {
-      image: "/images/gatsby-starter-cv.png",
-      description: "Gatsby starter CV template",
-      url: "https://www.gatsbyjs.org/starters/santosfrancisco/gatsby-starter-cv/"
+      image: "/images/chain-store-growth.png",
+      description: "ドミナント出店アニメーション",
+      url: "https://github.com/GangDuo/chain-store-growth"
     },
     {
-      image: "/images/awesome-grid.png",
-      description: "Responsive grid for ReactJS",
-      url: "https://github.com/santosfrancisco/react-awesome-styled-grid"
+      image: "/images/envelope-craftsman-js.png",
+      description: "カスタムロゴ封筒型紙",
+      url: "https://github.com/GangDuo/envelope-craftsman-js"
+    },
+    {
+      image: "/images/envelope-craftsman-js.png",
+      description: "送り状印刷",
+      url: "https://github.com/GangDuo/tag-for-tamago-js"
+    },
+    {
+      image: "/images/envelope-craftsman-js.png",
+      description: "Excel結合",
+      url: "https://github.com/gangduo/union-worksheet/"
+    },
+    {
+      image: "/images/envelope-craftsman-js.png",
+      description: "Excelオブジェクトの画像変換",
+      url: "https://github.com/GangDuo/excel-addin-shape-to-image"
+    },
+    {
+      image: "/images/excel-addin-barcode-gen.png",
+      description: "Excelバーコード一括生成",
+      url: "https://github.com/GangDuo/excel-addin-barcode-gen"
+    },
+    {
+      image: "/images/excel-addin-barcode-gen.png",
+      description: "Rデータ分析",
+      url: "https://github.com/GangDuo/goat"
     },
     /* more portifolio items here */
   ],
   social: {
-    twitter: "https://twitter.com/_franciscodf",
-    linkedin: "https://www.linkedin.com/in/santos-francisco",
-    github: "https://github.com/santosfrancisco",
-    email: "yoshi.df@gmail.com"
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/yoshiaki-onoda",
+    github: "https://github.com/GangDuo",
+    email: ""
   },
-  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
-  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteUrl: 'https://gangduo.github.io/',
+  pathPrefix: '', // Note: it must *not* have a trailing slash.
   siteCover: '/images/cover.jpeg',
-  googleAnalyticsId: 'UA-000000000-1',
+  googleAnalyticsId: 'UA-73379983-2',
   background_color: '#ffffff',
   theme_color: '#25303B',
   fontColor: "#000000cc",
